a6fe790849b1be629e458fa6fd63f6e3ac81ce7b
Zaire <110312083+zhar1@users.noreply.github.com>
2023-02-13 13:46:16 -0600

    makes playRound fuction that returns winner

--- myscripts.js
+++ myscripts.js
@@ -4,16 +4,36 @@ function getComputerChoice() {
     return choice[randomChoice];
 }
 
-function playRound(computerSelection, playerSelection){
+function playRound(playerSelection, computerSelection){
     const player = playerSelection;
     const computer = computerSelection;
     let result = '';
-    if (player.toLowerCase() === computer.toLowerCase){
-        result = 'Tie';
-    } 
-    //winning statements
-    //losing statements
+    switch (true) {
+        case (player.toLowerCase() === computer.toLowerCase()):
+            result = 'Tie';
+            break;
+        case ((player.toLowerCase() === 'rock') && (computer.toLowerCase() === 'scissors')):
+            result = 'You win! Rock beats scissors.';
+            break;
+        case ((player.toLowerCase() === 'paper') && (computer.toLowerCase() === 'rock')):
+            result = 'You win! Paper beats rock.';
+            break;
+        case ((player.toLowerCase() === 'scissors') && (computer.toLowerCase() === 'paper')):
+            result = 'You win! Scissors beats paper.';
+            break;
+        case ((computer.toLowerCase() === 'rock') && (player.toLowerCase() === 'scissors')):
+            result = 'You lose! Rock beats scissors.';
+            break;
+        case ((computer.toLowerCase() === 'paper') && (player.toLowerCase() === 'rock')):
+            result = 'You lose! Paper beats rock.';
+            break;
+        case ((computer.toLowerCase() === 'scissors') && (player.toLowerCase() === 'paper')):
+            result = 'You lose! Scissors beats paper.';
+            break;
+    }
+    return result;
 }
 
 const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
